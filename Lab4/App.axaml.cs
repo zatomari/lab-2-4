1d4354dec67c6490f90a18aa5cf45aca7027ebcf
Maria Kharisova <zatomari@ya.ru>
2024-11-24 21:05:37 +0300

    Cleanup

--- Lab4/App.axaml.cs
+++ Lab4/App.axaml.cs
@@ -1,12 +1,9 @@
-// namespace Lab4;
-
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
 
 using Lab4.ViewModels;
 using Lab4.Views;
-using Lab4.Vocabulary;
 
 public partial class App : Application {
     public override void Initialize() {
